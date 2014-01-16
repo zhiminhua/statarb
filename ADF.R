a5c09c228f241c6e08b8a08da25212eaeb5bf479
Zhimin.Hua <zhiminhua@gmail.com>
2014-01-16 12:53:45 +0800

    Improve ADF.R

--- ADF.R
+++ ADF.R
@@ -1,24 +1,26 @@
+# This program is used to conduct Augmented Dickey-Fuller test
 library(zoo)
 library(tseries)
 
 # Load the data
-gld <- read.csv("http://ichart.finance.yahoo.com/table.csv?s=GLD&ignore=.csv", stringsAsFactors=F)
-gdx <- read.csv("http://ichart.finance.yahoo.com/table.csv?s=GDX&ignore=.csv", stringsAsFactors=F)
+stock1 <- read.csv("http://ichart.finance.yahoo.com/table.csv?s=1398.HK&ignore=.csv", stringsAsFactors=F)
+stock2 <- read.csv("http://ichart.finance.yahoo.com/table.csv?s=0939.HK&ignore=.csv", stringsAsFactors=F)
 
-gld <- zoo(gld[,7], as.Date(gld[,1]))
-gdx <- zoo(gdx[,7], as.Date(gdx[,1]))
+stock1 <- zoo(stock1[,7], as.Date(stock1[,1]))
+stock2 <- zoo(stock2[,7], as.Date(stock2[,1]))
 
-t.zoo <- merge(gld, gdx, all=FALSE)
+t.zoo <- merge(stock1, stock2, all=FALSE)
 t <- as.data.frame(t.zoo)
 
 cat("Date range is", format(start(t.zoo)), "to", format(end(t.zoo)), "\n")
 
-m <- lm(gld ~ gdx + 0, data=t)
+# Calculate hedge ratio
+m <- lm(stock1 ~ stock2 + 0, data=t)
 beta <- coef(m)[1]
-
 cat("Assumed hedge ratio is", beta, "\n")
 
-sprd <- t$gld - beta*t$gdx
+# Perform ADF Test for cointegration
+sprd <- t$stock1 - beta*t$stock2
 ht <- adf.test(sprd, alternative="stationary", k=0)
 
 cat("ADF p-value is", ht$p.value, "\n")
